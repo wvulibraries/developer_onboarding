3b80d6af0170a1184a5c967f56f9ca284981fea6
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2016-03-21 10:38:04 -0700

    Adding another test for Variable Scope

--- ruby/coding-challenges/variable_scope.rb
+++ ruby/coding-challenges/variable_scope.rb
@@ -21,6 +21,40 @@ class VariableScope
   end
 end
 
+class Person
+  def initialize(name:)
+    self.name = name
+  end
+
+  def update_scenario_one(input)
+    self.name = input
+  end
+
+  def update_scenario_two(input)
+    name = input
+  end
+
+  def update_scenario_three(input)
+    @name = input
+  end
+
+  attr_reader :name
+
+  private
+
+  def name=(input)
+    @name = begin
+    case input
+    when String
+      input.upcase
+    when NilClass
+      input
+    when Symbol
+      input.to_s.upcase
+    end
+  end
+end
+
 # Write RSpec scenarios to state the expected output of each of the methods.
 #
 # In Ruby, when a class method is documented, we begin the method name with ".". An instance method begins with "#".
@@ -35,4 +69,12 @@ describe VariableScope do
 
   context '#class_variable'
   context '#instance_variable'
+end
+
+# Write RSpec scenarios that specify how `Person#name` is changed (or not changed).
+# Consider several scenarios of input; A string, a nil value, integer, and a symbol
+describe Person do
+  context '#update_scenario_one'
+  context '#update_scenario_two'
+  context '#update_scenario_three'
 end
